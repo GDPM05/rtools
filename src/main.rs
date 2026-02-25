c9ffa40764aadcbad7d37e52f41c9e9eb29263b4
GDPM05 <j.p.f.a009@gmail.com>
2026-02-25 20:21:57 +0000

    Base project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+
+use colored::Colorize;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+
+    // Removing the first argument "target\debug\rtools.exe"
+    args.remove(0);
+
+    if args.len() < 1 {
+        println!("{}", "Please specify the desired operation.".red());
+        return ();
+    }
+
+    if let Some(command) = args.get(0) {
+        match command.as_str() {
+            "echo" => {
+            },
+            "cat" => {
+
+            },
+            "ls" => {
+
+            },
+            "find" => {
+
+            },
+            "grep" => {
+
+            },
+            _ => {
+                println!("{}", "Invalid Operation!".red());
+            }
+        }
+    }
 }
+
